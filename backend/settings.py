4cba72a55b7c692902ccea24904a7cec2e656e36
reegine <regineangelina9@gmail.com>
2025-11-29 17:53:44 +0700

    fix api issue baseurl

--- backend/settings.py
+++ backend/settings.py
@@ -202,6 +202,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://subtympanitic-unpartaken-eula.ngrok-free.dev"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
